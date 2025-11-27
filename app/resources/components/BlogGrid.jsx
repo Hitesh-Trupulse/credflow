b8035f50afc8754e24a589ecc45ede72fa40b569
Hitesh Singh <hiteshsingh@rianinfotech.com>
2025-11-27 17:49:12 +0530

    made the resources view in list style

--- app/resources/components/BlogGrid.jsx
+++ app/resources/components/BlogGrid.jsx
@@ -94,7 +94,7 @@ export default function BlogGrid() {
       {/* Blog Cards */}
       {blogs.length > 0 && (
         <>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+          <div className="grid grid-cols-1 gap-6 md:gap-8">
             {getPaginatedBlogs().map((blog, index) => (
               <BlogCard key={index} blog={blog} />
             ))}
